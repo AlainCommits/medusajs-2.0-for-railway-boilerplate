181bea2569f61dcab87a1a1fe81ab2d695c68966
AlainCommits <alain.lilic1@gmail.com>
2024-09-12 00:26:38 +0200

    root

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -3,7 +3,7 @@ import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-primary">
+    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
@@ -19,11 +19,7 @@ const Hero = () => {
             Bisasam Shit
           </Heading>
         </span>
-       
-          <h1 style={{ textDecoration: "underline" }}>
-
-          </h1>
-        </a>
+     
       </div>
     </div>
   )
